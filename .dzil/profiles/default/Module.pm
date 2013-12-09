0b4b079d9a381ad7648108581a731b8f75fae8da
Sean Malloy <spinelli85@gmail.com>
2013-12-08 23:43:37 -0600

    Dist::Zilla minting profile. Frist attempt.

---
+++ .dzil/profiles/default/Module.pm
@@ -0,0 +1,127 @@
+package {{ $name }};
+
+# Copyright (c) 2013 {{ $dist->stash_named('%Rights')->copyright_holder }}. All rights reserved.
+# 
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+#    - Redistributions of source code must retain the above copyright
+#      notice, this list of conditions and the following disclaimer.
+#    - Redistributions in binary form must reproduce the above
+#      copyright notice, this list of conditions and the following
+#      disclaimer in the documentation and/or other materials provided
+#      with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+# COPYRIGHT HOLDERS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# ABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+use strict;
+use warnings;
+use English qw( -no_match_vars );
+require Exporter;
+
+our @ISA       = qw(Exporter);
+our @EXPORT_OK = qw( );
+our $VERSION   = '0.01';
+
+#############################################
+# Usage      : is_defined(scalar)
+# Purpose    : Test if a scalar is defined
+# Returns    : True if scalar is defined
+# Parameters : Scalar variable
+# Throws     : SPM::X::BadValue if scalar input is undef
+# Comments   : none
+# See Also   : perldoc -f defined, perldoc -f undef
+sub is_defined {
+    my $parameter = shift;
+    if (!defined $parameter) {
+        SPM::X::BadValue->throw({
+            ident   => 'bad parameter', 
+            tags    => [ qw(undef) ], 
+            public  => 1, 
+            message => "invalid parameter %{given_value}s for %{given_for}s", 
+            given_value => 'undef', 
+            given_for   => 'undef test', 
+        });
+    }
+    return 1;
+}
+
+1;
+__END__
+=head1 NAME
+
+{{ $name }} - Perl Module blah blah blah
+
+=head1 SYNOPSIS
+
+  use {{ $name }} qw( );
+
+=head1 DESCRIPTION
+
+The {{ $name }} blah blah blah
+
+=head2 EXPORT
+
+Nothing is exported by default from this module. All functions need to
+be explicitly requested.
+
+=head1 FUNCTIONS
+
+=head2 example(I<SCALAR>)
+
+Example blah blah blah
+
+=head1 SEE ALSO
+
+  blah blah blah
+
+=head1 BUGS
+
+No known bugs at this time.
+
+=head1 AUTHOR
+
+{{ $dist->stash_named('%Rights')->copyright_holder }}, E<lt>{{ $dist->stash_named('%User')->email }}E<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (c) 2013 {{ $dist->stash_named('%Rights')->copyright_holder }}. All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions
+are met:
+
+   - Redistributions of source code must retain the above copyright
+     notice, this list of conditions and the following disclaimer.
+   - Redistributions in binary form must reproduce the above
+     copyright notice, this list of conditions and the following
+     disclaimer in the documentation and/or other materials provided
+     with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+COPYRIGHT HOLDERS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
+
+=cut
+
