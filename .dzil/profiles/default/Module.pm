5d166e7d3d9dd1843d29091af38933b209c28936
Sean Malloy <spinelli85@gmail.com>
2014-01-02 19:28:55 -0600

    Add Dist::Zilla Pod::Weaver configuration.

--- .dzil/profiles/default/Module.pm
+++ .dzil/profiles/default/Module.pm
@@ -26,6 +26,8 @@ package {{ $name }};
 # ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 
+# ABSTRACT: please fill in this section
+
 use strict;
 use warnings;
 use English qw( -no_match_vars );
@@ -35,6 +37,13 @@ our @ISA       = qw(Exporter);
 our @EXPORT_OK = qw( );
 our $VERSION   = '0.01';
 
+=pod 
+
+=func example
+
+Example function.
+
+=cut
 #############################################
 # Usage      : example(scalar)
 # Purpose    : Test if a scalar is defined
@@ -43,78 +52,38 @@ our $VERSION   = '0.01';
 # Throws     : none
 # Comments   : none
 # See Also   : perldoc -f defined, perldoc -f undef
-#sub example {
-#    my $parameter = shift;
-#    if (!defined $parameter) {
-#        return;
-#    }
-#    return 1;
-#}
+sub example {
+    my $parameter = shift;
+    if (!defined $parameter) {
+        return;
+    }
+    return 1;
+}
 
 1;
-__END__
-=head1 NAME
 
-{{ $name }} - Perl Module blah blah blah
+=pod
 
 =head1 SYNOPSIS
 
-  use {{ $name }} qw( );
+  ...
 
 =head1 DESCRIPTION
 
-The {{ $name }} blah blah blah
+Put description here.
 
-=head2 EXPORT
+=head1 EXPORT
 
-Nothing is exported by default from this module. All functions need to
+Nothing is exported by defualt from this module. All functios need to
 be explicitly requested.
-
-=head1 FUNCTIONS
-
-=head2 example(I<SCALAR>)
-
-Example blah blah blah
 
 =head1 SEE ALSO
 
-  blah blah blah
+  ...
 
 =head1 BUGS
 
 No known bugs at this time.
-
-=head1 AUTHOR
-
-{{ $dist->stash_named('%Rights')->copyright_holder }}, E<lt>{{ $dist->stash_named('%User')->email }}E<gt>
-
-=head1 COPYRIGHT AND LICENSE
-
-Copyright (c) 2014 {{ $dist->stash_named('%Rights')->copyright_holder }}. All rights reserved.
-
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions
-are met:
-
-   - Redistributions of source code must retain the above copyright
-     notice, this list of conditions and the following disclaimer.
-   - Redistributions in binary form must reproduce the above
-     copyright notice, this list of conditions and the following
-     disclaimer in the documentation and/or other materials provided
-     with the distribution.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
-FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-COPYRIGHT HOLDERS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
-BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-ABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
-ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-POSSIBILITY OF SUCH DAMAGE.
 
 =cut
 
