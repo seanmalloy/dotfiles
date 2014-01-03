c284e50a8e2eb004c8c9d2c0721a8a1ec8f430f0
Sean Malloy <spinelli85@gmail.com>
2014-01-03 17:39:29 -0600

    Add Dist::Zilla Moose minting profile.

---
+++ .dzil/profiles/moose/Module.pm
@@ -0,0 +1,68 @@
+package {{ $name }};
+
+# Copyright (c) 2014 {{ $dist->stash_named('%Rights')->copyright_holder }}. All rights reserved.
+# 
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+#    - Redistributions of source code must retain the above copyright
+#      notice, this list of conditions and the following disclaimer.
+#    - Redistributions in binary form must reproduce the above
+#      copyright notice, this list of conditions and the following
+#      disclaimer in the documentation and/or other materials provided
+#      with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+# COPYRIGHT HOLDERS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# ABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+# ABSTRACT: please fill in this section
+
+use Moose;
+use English qw( -no_match_vars );
+
+our $VERSION = '0.01';
+
+=pod 
+
+=method example
+
+Example method.
+
+=cut
+has 'example' => (isa => 'Int', is => 'rw', required => 0, default => 0);
+
+__PACKAGE__->meta->make_immutable;
+
+1;
+
+=pod
+
+=head1 SYNOPSIS
+
+  ...
+
+=head1 DESCRIPTION
+
+Put description here.
+
+=head1 SEE ALSO
+
+  ...
+
+=head1 BUGS
+
+No known bugs at this time.
+
+=cut
+
