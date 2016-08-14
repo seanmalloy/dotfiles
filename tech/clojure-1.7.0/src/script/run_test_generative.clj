3241f8f7830b6e7accdfe080fdb9e83ad4ba9ad2
Sean Malloy <spinelli85@gmail.com>
2016-08-13 22:48:57 -0500

    Remove clojure

--- tech/clojure-1.7.0/src/script/run_test_generative.clj
+++
@@ -1,5 +0,0 @@
-(System/setProperty "java.awt.headless" "true")
-(when-not (System/getProperty "clojure.test.generative.msec")
-  (System/setProperty "clojure.test.generative.msec" "60000"))
-(require '[clojure.test.generative.runner :as runner])
-(runner/-main "test")
