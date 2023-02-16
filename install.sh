3834260f6af0ef8743b6cd151d4facbc1b42177d
Sean Malloy <smalloy@redhat.com>
2023-02-16 16:00:56 -0600

    Rebuild installer with real binaries

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:215904734e6311cce5cfb61ae6d1e78cf07cffa15d5267ca1065106da6c92a25
-size 26489178
+oid sha256:abda84ceb72a64fbf9c85042571ee7ecafc6de462958ad1008712443cddf6ffa
+size 55722202
