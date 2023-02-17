d1a33d44729e21691ea713596100ef797329a64d
Sean Malloy <spinelli85@gmail.com>
2023-02-16 22:10:47 -0600

    Update udev conifg for AVR programmer

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:abda84ceb72a64fbf9c85042571ee7ecafc6de462958ad1008712443cddf6ffa
-size 55722202
+oid sha256:edaaa46ba2107788a44cfa8bc37e26c09c582dfcb8ca75e6eef5368dd87064d8
+size 55717314
