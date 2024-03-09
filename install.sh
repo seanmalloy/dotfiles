7e3c80edce1faddb86ff3d254e5edc165e2cfae1
Sean Malloy <spinelli85@gmail.com>
2024-03-08 21:46:08 -0600

    Update to latest go present version

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dc3f9579b1dd2b2efbf263ab31bbf102951ea462fc2477ed519040a3e9019da6
-size 60301891
+oid sha256:f3fd5c803a257d6cf829648f050df9a72250084d5fd9664b7cbf47526d47c42e
+size 61138398
