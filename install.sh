07e47620930750bf1584ee4df6f70dcc21e73920
Sean Malloy <spinelli85@gmail.com>
2024-12-16 22:17:14 -0600

    Bump kind to v0.26.0

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c1c7012701600a2cdd9d7af93e468bae1e763dcf31f7876c8338fb1c1d459bd6
-size 63050444
+oid sha256:317c34f60059ae783c11c543f5c6df8eb2ebd7371aa3ca9666a9636025e6908a
+size 63229136
